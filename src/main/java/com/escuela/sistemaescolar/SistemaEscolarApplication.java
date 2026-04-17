8c5deb446a6087114238c8caf7499f377de74c96
Joanmm21 <joancitamm@hotmail.com>
2026-04-17 16:15:00 -0600

    actualizar la documentacion interna en clase principal para el desafio 2

--- src/main/java/com/escuela/sistemaescolar/SistemaEscolarApplication.java
+++ src/main/java/com/escuela/sistemaescolar/SistemaEscolarApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//la clase princial de arranque del proyecto para el desafio2
 public class SistemaEscolarApplication {
 
     public static void main(String[] args) {
