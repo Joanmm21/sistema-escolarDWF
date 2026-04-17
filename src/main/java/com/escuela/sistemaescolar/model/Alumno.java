5070444bb4ce427ca1d8a24ce561229e8c6a5ac7
Joanmm21 <joancitamm@hotmail.com>
2026-04-17 15:40:31 -0600

    implementar la clase entidad Alumno con relacion ManyToMany

--- src/main/java/com/escuela/sistemaescolar/model/Alumno.java
+++ src/main/java/com/escuela/sistemaescolar/model/Alumno.java
@@ -1,20 +1,27 @@
 package com.escuela.sistemaescolar.model;
+
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "alumno")
+@Data
+@NoArgsConstructor
 public class Alumno {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String nombre;
 
     @Column(nullable = false)
     private String apellido;
+
     @ManyToMany
     @JoinTable(
             name = "alumno_materia",
@@ -22,33 +29,4 @@ public class Alumno {
             inverseJoinColumns = @JoinColumn(name = "id_materia")
     )
     private Set<Materia> materias = new HashSet<>();
-
-    // Getters y Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public Set<Materia> getMaterias() {
-        return materias;
-    }
-    public void setMaterias(Set<Materia> materias) {
-        this.materias = materias;
-    }
 }
