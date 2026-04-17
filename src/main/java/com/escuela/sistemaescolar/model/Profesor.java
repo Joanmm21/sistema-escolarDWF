556a40d93e10b557526f6711cb41089895c350ed
Joanmm21 <joancitamm@hotmail.com>
2026-04-17 14:55:42 -0600

    implementar la clase entidad Profesor con anotaciones JPA

--- src/main/java/com/escuela/sistemaescolar/model/Profesor.java
+++ src/main/java/com/escuela/sistemaescolar/model/Profesor.java
@@ -1,27 +1,19 @@
 package com.escuela.sistemaescolar.model;
+
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "profesor")
+@Data
+@NoArgsConstructor
 public class Profesor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String nombre;
-
-    // Getters y setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
